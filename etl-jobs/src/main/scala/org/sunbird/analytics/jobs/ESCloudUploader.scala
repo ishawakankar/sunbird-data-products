e3c3dab97310df217296dc18698e0363bd46b35a
Isha Wakankar <iwakankar@hotmail.com>
2020-09-17 17:24:57 +0530

    .

--- etl-jobs/src/main/scala/org/sunbird/analytics/jobs/ESCloudUploader.scala
+++ etl-jobs/src/main/scala/org/sunbird/analytics/jobs/ESCloudUploader.scala
@@ -19,42 +19,49 @@ object ESCloudUploader {
 
         val index = config.getString("elasticsearch.query.index")
         val query = config.getString("elasticsearch.query.jsonString")
-        val outputFilePath = config.getString("outputFilePath")
+        // val outputFilePath = config.getString("outputFilePath")
 
         println(s"[$index] query ===> $query")
 
         require(!index.isEmpty && !query.isEmpty, "require valid inputs! index name and query cannot be empty!")
 
-        val conf = new SparkConf()
-            .setAppName("SparkEStoJSON")
+        // val conf = new SparkConf()
+        //     .setAppName("SparkEStoJSON")
+        //     .setMaster("local[*]")
+        //     // Elasticsearch settings
+        //     .set("es.nodes", config.getString("elasticsearch.host"))
+        //     .set("es.port", config.getString("elasticsearch.port"))
+        //     .set("es.scroll.size", config.getString("elasticsearch.scroll.size"))
+        //     .set("es.query", query)
+
+        // val sparkSession = SparkSession.builder.config(conf).getOrCreate
+        // val sc = sparkSession.sparkContext
+
+        // println(s"deleting output folder if exist! : $outputFilePath")
+        // val directory = new Directory(new File(outputFilePath))
+        // if (directory.exists) directory.deleteRecursively()
+
+        // sc.esJsonRDD(index).map(data => s"""{ "timestamp": ${now.getTime}, "data": ${data._2} }""")
+        //     .coalesce(1)
+        //     .saveAsTextFile(outputFilePath)
+
+        // // backup the output file to cloud
+        // val storageService = StorageServiceFactory.getStorageService(StorageConfig(config.getString("cloudStorage.provider"), config.getString("cloudStorage.accountName"), config.getString("cloudStorage.accountKey")))
+        // storageService.upload(config.getString("cloudStorage.container"), outputFilePath + "/part-00000", config.getString("cloudStorage.objectKey"), isDirectory = Option(false))
+        // println("successfully backed up file to cloud!")
+        
+
+        // VDN data from a separate es host
+        val outputVDNPath = config.getString("outputVDNPath")
+        val sparkConfig = conf
+        .setAppName("SparkEStoJSON")
             .setMaster("local[*]")
             // Elasticsearch settings
             .set("es.nodes", config.getString("elasticsearch.host"))
             .set("es.port", config.getString("elasticsearch.port"))
             .set("es.scroll.size", config.getString("elasticsearch.scroll.size"))
             .set("es.query", query)
-
-        val sparkSession = SparkSession.builder.config(conf).getOrCreate
-        val sc = sparkSession.sparkContext
-
-        println(s"deleting output folder if exist! : $outputFilePath")
-        val directory = new Directory(new File(outputFilePath))
-        if (directory.exists) directory.deleteRecursively()
-
-        sc.esJsonRDD(index).map(data => s"""{ "timestamp": ${now.getTime}, "data": ${data._2} }""")
-            .coalesce(1)
-            .saveAsTextFile(outputFilePath)
-
-        // backup the output file to cloud
-        val storageService = StorageServiceFactory.getStorageService(StorageConfig(config.getString("cloudStorage.provider"), config.getString("cloudStorage.accountName"), config.getString("cloudStorage.accountKey")))
-        storageService.upload(config.getString("cloudStorage.container"), outputFilePath + "/part-00000", config.getString("cloudStorage.objectKey"), isDirectory = Option(false))
-        println("successfully backed up file to cloud!")
-        
-
-        // VDN data from a separate es host
-        val outputVDNPath = config.getString("outputVDNPath")
-        val sparkConfig = conf
-            .set("es.nodes", config.getString("elasticsearch.host"))
+            // .set("es.nodes", config.getString("elasticsearch.host"))
 
         val vdnsparkSession = SparkSession.builder.config(sparkConfig).getOrCreate
         val sparkConf = vdnsparkSession.sparkContext
--- etl-jobs/src/scripts/DruidContentIndexer.sh
+++ etl-jobs/src/scripts/DruidContentIndexer.sh
@@ -14,38 +14,38 @@ jobJarPath="${home}/etl-jobs-1.0/etl-jobs-1.0.jar"
 jobConfPath="${home}/etl-jobs-1.0/resources/ESCloudUploader.conf"
 
 # get list of segments from content model snapshot datasource
-bkpIFS="$IFS"
-segmentIds=$(curl -X 'GET' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/metadata/datasources/${dataSourceName}/segments)
-IFS=',]['
-read -r -a array <<< ${segmentIds}
-IFS="$bkpIFS"
+# bkpIFS="$IFS"
+# segmentIds=$(curl -X 'GET' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/metadata/datasources/${dataSourceName}/segments)
+# IFS=',]['
+# read -r -a array <<< ${segmentIds}
+# IFS="$bkpIFS"
 
-# start the spark script to fetch Elasticsearch data and write it to a file and upload to cloud
-spark/bin/spark-submit \
---conf spark.driver.extraJavaOptions="-Dconfig.file=${jobConfPath}" \
---class org.ekstep.analytics.jobs.ESCloudUploader \
-${jobJarPath}
+# # start the spark script to fetch Elasticsearch data and write it to a file and upload to cloud
+# spark/bin/spark-submit \
+# --conf spark.driver.extraJavaOptions="-Dconfig.file=${jobConfPath}" \
+# --class org.ekstep.analytics.jobs.ESCloudUploader \
+# ${jobJarPath}
 
-printf "\n>>> submit ingestion task to Druid!\n"
+# printf "\n>>> submit ingestion task to Druid!\n"
 
-# submit task to start batch ingestion
-curl -X 'POST' -H 'Content-Type:application/json' -d @${ingestionSpecFilePath} http://${druidCoordinatorIP}:8090/druid/indexer/v1/task
+# # submit task to start batch ingestion
+# curl -X 'POST' -H 'Content-Type:application/json' -d @${ingestionSpecFilePath} http://${druidCoordinatorIP}:8090/druid/indexer/v1/task
 
 
-for index in "${!array[@]}"
-do
-    val=( $(eval echo ${array[index]}) )
-    printf "\n>>> Disabling segment id: $val \n"
-    # disable older segments
-    curl -X 'DELETE' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/datasources/${dataSourceName}/segments/$val
-done
+# for index in "${!array[@]}"
+# do
+#     val=( $(eval echo ${array[index]}) )
+#     printf "\n>>> Disabling segment id: $val \n"
+#     # disable older segments
+#     curl -X 'DELETE' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/datasources/${dataSourceName}/segments/$val
+# done
 
-printf "\n>>> Deleting segments from interval $interval \n"
+# printf "\n>>> Deleting segments from interval $interval \n"
 
-# delete older segments
-curl -X 'DELETE' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/datasources/${dataSourceName}/intervals/${interval}
+# # delete older segments
+# curl -X 'DELETE' -H 'Content-Type:application/json' http://${druidCoordinatorIP}:8081/druid/coordinator/v1/datasources/${dataSourceName}/intervals/${interval}
 
-printf "\n>>> success!\n"
+# printf "\n>>> success!\n"
 
 # get list of segments from vdn content model snapshot datasource
 bkpIFS="$IFS"
